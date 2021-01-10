0639fa2224b10e362651385c545972366204b0f7
Ervin <hamoydoerik@gmail.com>
2021-01-10 15:59:13 +0200

    Created project

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is my first project with React Native and ExpoJS</Text>
+      <Text>Somebody once told me the world is gonna roll me</Text>
       <StatusBar style="auto" />
     </View>
   );
